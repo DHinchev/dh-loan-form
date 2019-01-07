58c24d6f474d4b112bd41f0a57d1bd66828504a9
Daniel <d.r.hinchev@gmail.com>
2019-01-07 22:08:07 +0200

    make page redirection if no values are set

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import LoanForm from './components/loan-form';
 import LoanApproved from './components/loan-approved';
 import LoanDenied from './components/loan-denied';
  
+const renderApproval = () => {
+  const loanAmount = localStorage.getItem('loanAmount');
+  const loanTerm = localStorage.getItem('loanTerm');
+
+  if (!loanAmount || !loanTerm) {
+    return <Redirect to="/" />
+  }
+
+  return <LoanApproved loanAmount={loanAmount} loanTerm={loanTerm} />
+};
+
 const App = (props) => (
   <Router>
     <div className="App">
       <Switch>
-        <Route path="/approval" component={LoanApproved} />
+        <Route path="/approval" render={renderApproval} />
         <Route path="/denied" component={LoanDenied} />
         <Route exact path="/" component={LoanForm} />
       </Switch>
--- src/components/loan-approved.js
+++ src/components/loan-approved.js
@@ -1,44 +1,30 @@
-import React, { Component } from 'react';
+import '../styles/approved-component/approved.scss';
+
+import React from 'react';
 import ApprovedHead from './approved-head'; 
 import ApprovedSummaryContainer from './approved-summary-container'; 
-import '../styles/approved-component/approved.scss';
 import Data from '../assets/data/data.json';
 
-class LoanApproved extends Component {
-    
-    state = {
-        loanAmount: localStorage.getItem('loanAmount') || '',
-        loanTerm: localStorage.getItem('loanTerm') || ''
-    }
-
-    render() {
-        const {
-            loanAmount,
-            loanTerm
-        } = this.state;
-
-        return (
-            <div className="loan-approved">
-                <ApprovedHead 
-                    thankYouText={"We are happy to inform you that you are eligible for receiving funding up tp:"}
-                    loanAmount={loanAmount}
-                    loanTerm={loanTerm}
-                    upTo={"for up to"}
-                />
-                <ApprovedSummaryContainer 
-                    loanAmount={loanAmount}
-                    loanTerm={loanTerm}
-                />
-                <p className="loan-approved-apply-info">{Data.approvalApplyInfoText}</p>
-                <div className="loan-approved-button-container">
-                    <button className="loan-approved-submit-button" type="submit">
-                        Apply now
-                    </button>
-                </div>
-                <p className="loan-approved-interest-details">{Data.loanApprovedInterestDetailsText}</p>
-            </div>
-        );
-    }
-}
+const LoanApproved = ({loanAmount, loanTerm}) => (
+    <div className="loan-approved">
+        <ApprovedHead 
+            thankYouText="We are happy to inform you that you are eligible for receiving funding up to:"
+            loanAmount={loanAmount}
+            loanTerm={loanTerm}
+            upTo="for up to"
+        />
+        <ApprovedSummaryContainer 
+            loanAmount={loanAmount}
+            loanTerm={loanTerm}
+        />
+        <p className="loan-approved-apply-info">{Data.approvalApplyInfoText}</p>
+        <div className="loan-approved-button-container">
+            <button className="loan-approved-submit-button" type="submit">
+                Apply now
+            </button>
+        </div>
+        <p className="loan-approved-interest-details">{Data.loanApprovedInterestDetailsText}</p>
+    </div>
+);
 
 export default LoanApproved;
